6d770dba9cba224788121537f46a58d353a7a7f9
Souravkumarverma123 <souravkumarverma56@gmail.com>
2025-10-13 02:13:03 +0530

    user model and video model created

---
+++ src/app.js
@@ -0,0 +1,38 @@
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+const app = express()
+// we uses app.use when we have do a middleware or any configuration related settings
+
+app.use(cors({
+   origin: process.env.CORS_ORIGIN,
+   credentials: true
+}))
+
+ app.use(express.json({limit: "20kb"}))
+app.use(express.urlencoded({extended: true, limit: '20kb'}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export { app }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
     path: './env'
 })
 
-
+const port = process.env.PORT || 8000;
 connectDB()
+.then(() => {
+    app.on("error",(error) => {
+        console.log("Error: ", error);
+        throw error 
+    })
+    app.listen(port, () => {
+        console.log(`server is running at port :${port}`);
+    })
+})
+.catch((error) =>{
+    console.log("MONGO DB connection failed !!! ", error);
+    
+})
+
 
 
 
